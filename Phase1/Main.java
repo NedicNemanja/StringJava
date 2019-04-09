7d8976bfadc985f5753cd3cdcb51a6386982f800
NedicNemanja <sdi1400124@di.uoa.gr>
2019-04-09 23:58:54 +0300

    removing redundant files/dirs

--- Phase1/Main.java
+++
@@ -1,9 +0,0 @@
-import java_cup.runtime.*;
-import java.io.*;
-
-class Main {
-    public static void main(String[] argv) throws Exception{
-        Parser p = new Parser(new Scanner(new InputStreamReader(System.in)));
-        p.parse();
-    }
-}
